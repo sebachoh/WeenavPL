cea97cf525ea9e7b6593bd8b6413fda97994cfe6
SEBASTIAN RUIZ <SEBARUIZ01>
2026-02-04 00:16:29 +0100

    1st day is over! tomorrow ill learn a lot and work towards the front end :)

--- frontend/src/components/BoatList.jsx
+++ frontend/src/components/BoatList.jsx
@@ -1,7 +1,14 @@
 export default function BoatList() {
     return (
-        <div className="flex-1 h-[calc(100vh-2rem)] m-4 bg-white flex items-center justify-center p-6 rounded-3xl border-r border-gray-400">
-            <h1 className="text-4xl font-bold">Panel qui est a droite</h1>
+        <div className="flex-1 h-[calc(100vh-2rem)] m-4 bg-white flex items-center justify-center p-6 rounded-3xl shadow-xl shadow-slate-200/60 border border-slate-100">
+            <h1 className="text-4xl font-bold text-center">Panel qui est a droite
+                <br />
+                Bonjour, je suis en train d'être développé
+                <br />
+
+                Merci de votre patience
+            </h1>
+
         </div>
     );
 }
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,35 +2,38 @@ import logo from "../assets/img/LogoEntreprise.png";
 
 export default function Sidebar() {
     return (
-        <div className="w-1/4 h-[calc(100vh-2rem)] m-4 bg-black flex flex-col justify-between p-6 rounded-3xl border-r border-gray-400">
+        <div className="w-1/4 h-[calc(100vh-2rem)] m-4 bg-black flex flex-col justify-between p-6 rounded-3xl shadow-2xl shadow-black/50 border border-white/5">
 
             <div>
                 <div className="mb-10 flex flex-col items-center">
                     <div className="w-56 h-20 bg-white rounded-3xl flex items-center justify-center p-4 shadow-lg overflow-hidden hover:scale-105 transition-transform">
                         <img src={logo} alt="Logo" className="w-full h-full object-contain" />
                     </div>
-                    <h1 className="text-4xl font-bold text-slate-500 pt-4 text-center">
-                        W <span className="text-2xl text-white">Dashboard</span>
+                    <h1 className="text-4xl font-bold text-slate-500 pt-4 text-center tracking-[-0.05em]">
+                        W <span className="text-2xl text-white tracking-[-0.05em]">Dashboard</span>
                     </h1>
-                    <p className="text-gray-400 text-center">Gestion de flotte</p>
+                    <p className="text-gray-400 text-center tracking-[-0.05em]">Système de monitorage</p>
                 </div>
 
                 <nav className="space-y-4 text-white">
                     <p className="text-xs font-semibold text-slate-500 uppercase tracking-wider">Menu</p>
-                    <button className="w-full text-left p-2 rounded-2xl hover:bg-slate-800 transition-colors">
-                        <span className="text-sm font-medium pl-2">Boton sample :D</span>
+                    <button className="w-full text-left p-2 rounded-2xl hover:bg-slate-800 hover:scale-105 transition-transform">
+                        <span className="text-sm font-medium tracking-[-0.05em]">Liste des bateaux 🛥️</span>
+                    </button>
+                    <button className="w-full text-left p-2 rounded-2xl hover:bg-slate-800 hover:scale-105 transition-transform">
+                        <span className="text-sm font-medium tracking-[-0.05em]">Liste des clients 📲</span>
                     </button>
                 </nav>
             </div>
 
-            <div className="border-t border-slate-700 pt-4 text-white">
+            <div className="border-t border-slate-700 pt-4 text-white hover:scale-105 transition-transform">
                 <div className="flex items-center gap-3">
                     <div className="w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center font-bold">
                         S
                     </div>
                     <div>
-                        <p className="text-sm font-medium">Sebastian Ruiz</p>
-                        <p className="text-xs text-slate-500">Admin</p>
+                        <p className="text-sm font-medium tracking-[-0.05em]">Sebastián Ruiz - Stage chez Weenav</p>
+                        <p className="text-xs text-slate-500 tracking-[-0.05em]">Admin</p>
                     </div>
                 </div>
             </div>
