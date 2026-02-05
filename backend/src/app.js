a8a32f04b6187f7ec4989c8b7211965feb530b05
SEBASTIAN RUIZ <SEBARUIZ01>
2026-02-05 16:01:03 +0100

    checkpoint before starting with the connection between BACK and FRONT \ added CORS in back

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,10 @@ import express from "express";
 import { PORT } from "./config.js";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(usersRoutes);
